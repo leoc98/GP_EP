c8eef375ba41485a2a341b8ff6dc90b6ee4c89e7
leocao <949427982@qq.com>
2025-03-31 20:35:01 -0700

    [feature] add BBL observation

--- src/EpistemicModel/epistemic_seeing_rule.h
+++ src/EpistemicModel/epistemic_seeing_rule.h
@@ -4,6 +4,8 @@
 #include "epistemic_common.h"
 #include "../state_descriptor.h"
 #include <map>
+#include <unordered_map>
+#include <cmath>
 
 
 class EpistemicSeeingRule {
@@ -114,4 +116,141 @@ public:
     }
 };
 
+class BBLSeeingRule : public EpistemicSeeingRule {
+public:
+    bool checkVisibility(
+        const StateType::predicates& state, 
+        const epistemic::agent& agent_name, 
+        const string& target_predicate_name,
+        const StateType::parameter_list& param_list,
+        const vector<string>& param_names,
+        const map< pair< string, int> , string >& id_to_obj_name,
+        const map< string, int >& obj_to_address,
+        StateDescriptor* sd
+    ) {
+        if (target_predicate_name == "cw_cnct@nt" || 
+            target_predicate_name == "ccw_cnct@nt" ||
+            target_predicate_name == "agent_at@nt" ||
+            target_predicate_name == "view_at@nt"
+            )
+        {
+            return true;
+        }
+        auto find_loc_by_obj_ind = [&sd, &id_to_obj_name, &obj_to_address, &state](const string& pred_type, const int& obj_ind) -> pair<int, int> {
+            StateType::parameter_list pred_param_list(sd->getPredicateVarNamesNumber(pred_type));
+            pred_param_list[0] = obj_ind;
+
+            // check each room fullfill pred or not
+            int pos_x = 0;
+            const string x_pos_type = "x_pos";
+            const string y_pos_type = "y_pos";
+            // bool find_pos = false;
+            while (id_to_obj_name.count({x_pos_type, pos_x})) {
+                const string& xpos_name = id_to_obj_name.at({x_pos_type, pos_x});
+                pred_param_list[1] = obj_to_address.at(xpos_name);
+                int pos_y = 0;
+                while (id_to_obj_name.count({y_pos_type, pos_y})) {
+                    const string& ypos_name = id_to_obj_name.at({y_pos_type, pos_y});
+                    pred_param_list[2] = obj_to_address.at(ypos_name);
+                    if (state[sd->getPredicateIDX(pred_type)].at(pred_param_list) == 1)
+                    {
+                        // find_pos = true;
+                        return {pos_x,pos_y};
+                    }
+                    pos_y++;
+                }
+                pos_x++;
+            }
+            // assert(find_pos);
+            return {-1,-1};
+        };
+        // get target loc
+        string target_name = id_to_obj_name.at({param_names[0],param_list[0]});
+        auto target_pos = find_loc_by_obj_ind(target_predicate_name.find("agent")!=string::npos?"agent_at@nt":"view_at@nt", obj_to_address.at(target_name));
+        int& target_x = target_pos.first; 
+        int& target_y = target_pos.second; 
+        if (target_x == -1 && target_y == -1) {
+            return false;
+        }
+        // get agent loc
+        auto agent_pos = find_loc_by_obj_ind("agent_at@nt", obj_to_address.at(agent_name));
+        int& agent_x = agent_pos.first; 
+        int& agent_y = agent_pos.second;
+
+        if (target_x == agent_x && target_y == agent_y) {
+            return true;
+        }
+
+        int dx = target_x - agent_x;
+        int dy = target_y - agent_y;
+
+        auto find_agt_dir = [&sd, &id_to_obj_name, &obj_to_address, &state](const string& pred_type, const int& agent_ind) {
+            vector<string> pred_param_names = sd->getPredicateVarNames(pred_type);
+            assert(pred_param_names.size() == 2);
+            StateType::parameter_list pred_param_list(pred_param_names.size());
+            pred_param_list[0] = agent_ind;
+
+            // check each room fullfill pred or not
+            vector<string> dir_list = {"n", "ne", "e", "se", "s", "sw", "w", "nw"};
+            for (const string& dir : dir_list) {
+                pred_param_list[1] = obj_to_address.at(dir);
+                if (state[sd->getPredicateIDX(pred_type)].at(pred_param_list) == 1)
+                {
+                    return dir;
+                }
+            }
+            // assert(false);
+            return string("unknown");
+        };
+        string dir = find_agt_dir("agent_face", obj_to_address.at(agent_name));
+
+        if (dir == string("unknown")) {
+            return false;
+        }
+        if (dx == dy) {
+            if (dx > 0) {
+                return (dir == "n") || (dir == "ne") || (dir == "e");
+            } else if (dx < 0) {
+                return (dir == "s") || (dir == "sw") || (dir == "w");
+            }
+        }
+
+        if (dx == -dy) {
+            if (dx > 0) {
+                return (dir == "e") || (dir == "se") || (dir == "s");
+            } else if (dx < 0) {
+                return (dir == "w") || (dir == "nw") || (dir == "n");
+            }
+        }
+
+        const std::unordered_map<std::string, std::pair<int, int>> directions = {
+            {"n", {45, 135}},
+            {"ne", {0, 90}},
+            {"e", {-45, 45}},
+            {"se", {-90, 0}},
+            {"s", {-135, -45}},
+            {"sw", {-180, -90}},
+            {"w", {-135, 135}},
+            {"nw", {90, 180}}
+        };
+
+        // Calculate the angle of the target relative to the agent.
+        double angle = std::atan2(dy, dx) * 180 / M_PI;
+        if (angle > 180) {
+            angle -= 360;
+        } else if (angle < -180) {
+            angle += 360;
+        }
+
+        // Check if the target is within the 90 - degree viewing angle of the agent's direction.
+        assert(directions.find(dir) != directions.end());
+        int min_angle = directions.at(dir).first;
+        int max_angle = directions.at(dir).second;
+        if (dir == "w") {
+            return (angle <= min_angle || angle >= max_angle);
+        }
+            
+        return (angle >= min_angle && angle <= max_angle);
+    }
+};
 #endif // EPISTEMIC_SEEING_RULE_H
